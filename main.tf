454463c6e1e7e487e46f9a08a76652186e62f5dc
gopi <gopi199503@gmail.com>
2026-02-20 08:48:17 +0530

    terraform code added

---
+++ main.tf
@@ -0,0 +1,25 @@
+resource "google_compute_instance" "default" {
+  name         = var.instance_name
+  machine_type = var.machinetype
+  zone         = var.zones
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+      size = var.disk_size
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.my_vpc.id
+
+    access_config {
+
+    }
+  }
+  
+}
+
+resource "google_storage_bucket" "name" {
+  
+}
